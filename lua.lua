ed379cd10d21abb6ddb6c0494c9cd3bb20cd771e
yigoza <79945567+yigoza@users.noreply.github.com>
2022-05-21 22:11:39 +0300

    Update lua.lua

--- lua.lua
+++ lua.lua
@@ -1,7 +1,7 @@
 IPlist =
     {
         {"avenger", "217.195.195.42"},  --upgrade   
-        {"yigoza", "217.195.207.199"},  --upgrade  
+        {"yigoza", "217.195.207.21"},  --upgrade  
     }
 setElementData(root, "koruma", 1)
 oneat = nil
